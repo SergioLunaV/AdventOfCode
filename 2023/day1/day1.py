49c0b67e96074e12664b1706d84725bc1bc116ed
Sergio Luna Puerto <i12lupus@uco.es>
2023-12-02 02:07:24 +0100

    Challenge of Day 1 successly completed

--- 2023/day1/day1.py
+++ 2023/day1/day1.py
@@ -1,23 +1,49 @@
+# Save the file's data
 file = open("input", "r")
 lines = file.readlines()
 file.close()
 
-
+spelled_numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 addition = 0
 
 for line in lines:
-    first_digit = ""
-    last_digit = ""
+    position_number = {}
 
-    for character in line :
-        if character.isdigit() and first_digit == "":
-            first_digit = character
-            last_digit = character
+    # Convert words to numbers and save in the dictionary
+    for spelled_number in spelled_numbers:
+        # First occurence
+        index = line.find(spelled_number)
+        if index != -1:
+            position_number[index] = spelled_numbers.index(spelled_number)
+        
+        # Last occurrence
+        index = line.rfind(spelled_number)
+        if index != -1:
+            position_number[index] = spelled_numbers.index(spelled_number)
+        
+    # Search digit numbers
+    index = 0
+    for character in line:
+        if character.isdigit():
+            position_number[index] = character
+        
+        index += 1
 
-        elif character.isdigit() and first_digit != "":
-            last_digit = character
+    # Search the first and last
+    min_index = float('inf')
+    max_index = float('-inf')
+    first_number = ""
+    last_number = ""
 
-    addition += int(str(first_digit) + str(last_digit))
+    for index, number in position_number.items():
+        if index < min_index:
+            min_index = index
+            first_number = number
+        if index > max_index:
+            max_index = index
+            last_number = number
+        
+    addition += int(str(first_number) + str(last_number))
 
 print(addition)
