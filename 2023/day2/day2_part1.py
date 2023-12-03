5188ac0d8162fcc1a71ea1902229dc0d348925a7
Sergio Luna Puerto <i12lupus@uco.es>
2023-12-03 17:13:31 +0100

    Challenged completed

--- 2023/day2/day2_part1.py
+++ 2023/day2/day2_part1.py
@@ -4,20 +4,50 @@ MAX_GREEN  = 13
 MAX_BLUE = 14
 
 # Read the file
-inputFile = open("example_input", "r")
+inputFile = open("puzzle_input", "r")
 text = inputFile.readlines()
 inputFile.close()
 
-game_sum = 0
+game_id_sum = 0
 
 # Read each line
 for line in text:
    # Separate the game and the sets
-    splited_game = line.split(':')
-    game = splited_game[0]
-    sets = splited_game[1]
+    splitted_game = line.split(':')
+    game = splitted_game[0].split(' ')
+    sets = splitted_game[1].split(';') # Separated sets
 
-    game_id = game[len(game)-1] # Take the game's id
+    game_id = game[1]    
+    valid_game = True
 
-    # Separate the sets 
-    splited_sets = sets.split(';')
+    for set in sets:
+        # Separate each get of cubes
+        subsets = set.split(',')
+
+        red_count = 0
+        green_count = 0
+        blue_count = 0
+
+        for subset in subsets:
+            # Separate each cube
+            splitted_subset = subset.split(' ')
+            
+            cube_quantity = splitted_subset[1]
+
+            cube_color = splitted_subset[2].strip("\n")
+
+            if(cube_color == "red"):
+                red_count += int(cube_quantity)
+            elif(cube_color == "green"):
+                green_count += int(cube_quantity)
+            elif(cube_color == "blue"):
+                blue_count += int(cube_quantity)
+                
+        if(red_count > MAX_RED) or (green_count > MAX_GREEN) or (blue_count > MAX_BLUE):
+            valid_game = False
+            break
+
+    if(valid_game == True):
+        game_id_sum += int(game_id)
+
+print("The sum of the ID's of the valid games is: ", game_id_sum)
